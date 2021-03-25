86b395b2d0f4434c4dd8f9d2fcda7648a1ac1be7
mohamedotaki <52335186+mohamedotaki@users.noreply.github.com>
2021-03-25 17:55:57 +0000

    created main and database class

---
+++ src/main/java/ie/gmit/DataBase.java
@@ -0,0 +1,21 @@
+package ie.gmit;
+
+import java.util.ArrayList;
+
+public class DataBase {
+    public ArrayList<String> availableSkills(){
+        ArrayList<String> availableSkills = new ArrayList<>();
+        availableSkills.add("Communication");
+        availableSkills.add("Computer");
+        availableSkills.add("People");
+        availableSkills.add("Leadership");
+        availableSkills.add("Organizational");
+        availableSkills.add("Time management");
+        availableSkills.add("Collaboration");
+        availableSkills.add("Problem-solving");
+        availableSkills.add("Coding");
+        return availableSkills;
+    }
+
+
+}
