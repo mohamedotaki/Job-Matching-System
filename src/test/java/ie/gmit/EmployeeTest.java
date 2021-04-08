7cc6dc526a06b493f11ec3d5c21b22b3fe565079
Tacklbac <small.rick.y@gmail.com>
2021-04-08 01:14:43 +0100

    Ricky Main Test

--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static jdk.nashorn.internal.objects.Global.println;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -22,6 +23,33 @@ public class EmployeeTest {
     }
 
     @Test
+    void newEmployeeNoSkills(){
+        employee = new Employee(1,"Mr","mohamed","g00346067@gmit.ie","Password2",
+                "0888888888","mayo","Software and Electronic Engineering Year1");
+
+    }
+
+    @Test
+    void testAddSkillTrue() {
+        skill = new Skill("Organizational Skill",5);
+        skills.add(skill);
+        employee.setSkills(skills);
+        assertEquals(2,employee.getSkills().size());
+
+    }
+//    @Test
+//    void testAddSkillFalse() {
+//        skill = null;
+//        assertThrows(IllegalArgumentException.class,);
+//    }
+
+    @Test
+    void testRemoveSkillTrue() { }
+    @Test
+    void testremoveSkillFalse() { }
+
+
+    @Test
     void testSetDegreeFalse(){
         assertThrows(IllegalArgumentException.class,()->employee.setDegree("Error"));
     }
@@ -29,6 +57,7 @@ public class EmployeeTest {
     void testSetDegreeTrue(){
         assertEquals("Software and Electronic Engineering",employee.getDegree());
     }
+
     @Test
     void testSetSkillsFalse(){
         ArrayList<Skill> emptySkills = new ArrayList<>();
